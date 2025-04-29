5d7af509cc32cb803728cee711c0820b387dfba9
Maxime Trimolet <maxime.trimolet@gmail.com>
2025-04-29 14:35:14 +0200

    add an App module

--- src/main.cpp
+++ src/main.cpp
@@ -2,59 +2,14 @@
 #include <unistd.h>
 
 import std;
-import glm;
-import stormkit.core;
-import ncurses;
+import consoleapp;
 
-import geometry;
-import grid;
-
-import xmlparser;
+using App = ConsoleApp;
 
 auto main(std::span<const std::string_view> args) noexcept -> int {
-  // [tmpfix] remove when stormkit userMain allows for non-packaged build
+  // [tmpfix] remove when xmake target properly handles workdir on macos
   chdir("/Users/mtrimolet/Desktop/mtrimolet/hroza");
 
-  auto&& window = ncurses::window{};
-
-  auto&& palette = xmlparser::parseXmlPalette("./resources/palette.xml");
-  auto&& modelfile = std::ranges::size(args) >= 2 ? args[1] : "./models/DenseSAW.xml";
-  window.say(std::format("Loading model {}", modelfile));
-  auto&& model = xmlparser::parseXmlModel(modelfile);
-
-  auto&& grid = TracedGrid{std::dims<3>{1u, std::get<0>(window.getmaxyx()), std::get<1>(window.getmaxyx())}, model.symbols[0]};
-  if (model.origin) grid[(grid.area() / 2u).outerbound()] = model.symbols[1];
-
-  window.setpalette(model.symbols
-    | std::views::transform([&palette](auto&& s) noexcept {
-        return palette.contains(s) 
-          ? palette.at(s) 
-          : 0xffffff;
-    })
-    | std::ranges::to<std::vector>()
-  );
-
-  auto&& offsets = std::views::zip(model.symbols, std::views::iota(0u))
-    | std::ranges::to<std::unordered_map<char, unsigned int>>();
-
-  auto&& addch = [&window, &offsets](auto&& _value) noexcept {
-    auto&& [u, value] = _value;
-    if (window.hascolors()) {
-      auto&& offset = offsets.contains(value) ? offsets.at(value) : 0;
-      window.addch(u.y, u.x, value, offset);
-    } else {
-      window.addch(u.y, u.x, value);
-    }
-  };
-
-  std::ranges::for_each(std::views::zip(mdiota(grid.area()), grid), addch);
-  window.refresh();
-  window.waitchar();
-
-  for (auto&& changes : model.program(grid)) {
-    std::ranges::for_each(changes, addch);
-    window.refresh();
-  }
-
-  return 0;
+  auto app = App{};
+  return app.run(args);
 }
