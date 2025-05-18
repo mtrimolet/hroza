543ac40b5b20731a891bee78961f8e88d84fd5ba
Maxime Trimolet <maxime.trimolet@gmail.com>
2025-05-18 13:15:41 +0200

    improve layout remove junk

--- src/consoleapp.cpp
+++ src/consoleapp.cpp
@@ -5,13 +5,6 @@ using namespace ftxui;
 
 using Palette = std::unordered_map<char, Color>;
 
-inline constexpr auto update_texture(Image& img, const Palette& palette, const std::tuple<math::Vector3U, char>& value) noexcept -> decltype(auto) {
-  auto&& [u, character] = value;
-  auto&& pixel = img.PixelAt(u.x, u.y);
-  pixel.character = character;
-  pixel.background_color = palette.at(character);
-}
-
 auto ConsoleApp::run([[maybe_unused]] std::span<const std::string_view> args) noexcept -> int {
   load_palette(DEFAULT_PALETTE_FILE);
 
@@ -35,10 +28,9 @@ auto ConsoleApp::run([[maybe_unused]] std::span<const std::string_view> args) no
     std::views::zip(
       model.symbols,
       mdiota(std::dims<3>{1, palette_texture.dimy(), palette_texture.dimx()})
-    ) | std::views::transform([](auto&& p) static noexcept { auto&& [a, b] = p; return std::make_pair(b, a); }),
-    // bindFront(update_texture, palette_texture, model_palette)
+    ),
     [&](auto&& cu) noexcept {
-      auto&& [u, character] = cu;
+      auto&& [character, u] = cu;
       auto&& pixel = palette_texture.PixelAt(u.x, u.y);
       pixel.character = character;
       pixel.background_color = model_palette.at(character);
@@ -51,15 +43,15 @@ auto ConsoleApp::run([[maybe_unused]] std::span<const std::string_view> args) no
     static_cast<int>(grid.extents.extent(2)),
     static_cast<int>(grid.extents.extent(1))
   };
-
-  // auto update_grid_texture = bindFront(update_texture, grid_texture, model_palette);
-  auto update_grid_texture = [&](auto&& c) noexcept {
-    auto&& [u, character] = c;
-    auto&& pixel = grid_texture.PixelAt(u.x, u.y);
-    pixel.character = character;
-    pixel.background_color = model_palette.at(character);
+  auto update_grid_texture = [&](auto&& changes) noexcept {
+    std::ranges::for_each(changes, [&](auto&& change) noexcept {
+      auto&& [u, character] = change;
+      auto&& pixel = grid_texture.PixelAt(u.x, u.y);
+      pixel.character = character;
+      pixel.background_color = model_palette.at(character);
+    });
   };
-  std::ranges::for_each(std::views::zip(mdiota(grid.area()), grid), update_grid_texture);
+  update_grid_texture(std::views::zip(mdiota(grid.area()), grid));
 
   auto screen = ScreenInteractive::Fullscreen();
   auto main_loop = Loop{&screen, Renderer([&]() noexcept {
@@ -67,7 +59,8 @@ auto ConsoleApp::run([[maybe_unused]] std::span<const std::string_view> args) no
       canvas(grid_texture.dimx(), grid_texture.dimy(),
              bindBack(&Canvas::DrawImage, 0, 0, grid_texture))
         | size(WIDTH, GREATER_THAN, grid_texture.dimx())
-        | size(HEIGHT, GREATER_THAN, grid_texture.dimy()),
+        | size(HEIGHT, GREATER_THAN, grid_texture.dimy())
+        | center | flex_grow,
       separator(),
       vbox({
         window(text("symbols"), canvas(
@@ -79,19 +72,10 @@ auto ConsoleApp::run([[maybe_unused]] std::span<const std::string_view> args) no
     }));
   })};
 
-  // for (auto&& changes : model.program(grid)) {
-  //   if (main_loop.HasQuitted()) break;
-  //   std::ranges::for_each(changes, [&](auto&& c) noexcept { auto&& [u, character] = c; update_grid_texture(std::make_tuple(u, character)); });
-  //   main_loop.RunOnce();
-  //   // std::this_thread::yield();
-  //   // std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  // }
-
   auto program_thread = std::jthread([&](std::stop_token stop) mutable noexcept {
-    std::ranges::for_each(std::views::zip(mdiota(grid.area()), grid), update_grid_texture);
     for (auto&& changes : model.program(grid)) {
       if (stop.stop_requested()) return;
-      std::ranges::for_each(changes, [&](auto&& c) noexcept { auto&& [u, character] = c; update_grid_texture(std::make_tuple(u, character)); });
+      update_grid_texture(changes);
       screen.RequestAnimationFrame();
     }
   });
