943471e1b14f46a46279386fcb4dd500160493c9
Maxime Trimolet <maxime.trimolet@gmail.com>
2025-05-23 15:38:07 +0200

    found a way for program tree

--- src/consoleapp.cpp
+++ src/consoleapp.cpp
@@ -3,6 +3,10 @@ module consoleapp;
 import stormkit.core;
 import geometry;
 
+import executionnode;
+import rulenode;
+import rewriterule;
+
 using namespace std::string_literals;
 using namespace stormkit;
 using namespace ftxui;
@@ -15,7 +19,84 @@ inline constexpr auto canvasFromImage(const Image& img) noexcept -> decltype(aut
     | size(HEIGHT, GREATER_THAN, img.dimy());
 }
 
-auto ConsoleApp::run([[maybe_unused]] std::span<const std::string_view> args) noexcept -> int {
+inline constexpr auto rule(const RewriteRule& rule, const Palette& palette) noexcept -> Element {
+  auto input = Image{
+    static_cast<int>(rule.input.extents.extent(2)),
+    static_cast<int>(rule.input.extents.extent(1))
+  };
+  auto output = Image{
+    static_cast<int>(rule.output.extents.extent(2)),
+    static_cast<int>(rule.output.extents.extent(1))
+  };
+
+  std::ranges::for_each(
+    std::views::zip(mdiota(rule.input.area()), rule.input, rule.output),
+    [&input, &output, &palette](auto&& uio) noexcept {
+      auto&& [u, i, o] = uio;
+
+      auto&& ip = input.PixelAt(u.x, u.y);
+      // TODO input should be char refering to a set, not a set ; the indirection is required
+      ip.character = i ? '?' : '*';
+      // ip.character = i.value_or('*');
+      // ip.background_color = palette.at(i);
+
+      auto&& op = output.PixelAt(u.x, u.y);
+      op.character = o.value_or('*');
+      op.background_color = o and palette.contains(*o) ? palette.at(*o) : Color::Default;
+    }
+  );
+
+  return hbox({
+    canvasFromImage(input) | border,
+    text("->") | vcenter,
+    canvasFromImage(output) | border,
+  });
+}
+
+inline constexpr auto ruleNode(const Action& node, const Palette& palette, std::optional<UInt> count = {}) noexcept -> Element {
+  if (const auto one = node.target<One>(); one != nullptr) {
+    auto nodes = Elements{text("one")};
+    if (count) nodes.push_back(text(std::format("({}/?)", *count)));
+    nodes.append_range(one->rules | std::views::transform(bindBack(rule, palette)));
+    return vbox(nodes);
+  }
+  if (const auto all = node.target<All>(); all != nullptr) {
+    auto nodes = Elements{text("all")};
+    if (count) nodes.push_back(text(std::format("({}/?)", *count)));
+    nodes.append_range(all->rules | std::views::transform(bindBack(rule, palette)));
+    return vbox(nodes);
+  }
+  if (const auto prl = node.target<Prl>(); prl != nullptr) {
+    auto nodes = Elements{text("prl")};
+    if (count) nodes.push_back(text(std::format("({}/?)", *count)));
+    nodes.append_range(prl->rules | std::views::transform(bindBack(rule, palette)));
+    return vbox(nodes);
+  }
+  return text("<unknown_rule_node>");
+}
+
+inline constexpr auto executionNode(const ExecutionNode& node, const Palette& palette) noexcept -> Element {
+  if (const auto markov = node.target<Markov>(); markov != nullptr) {
+    auto nodes = Elements{text("markov")};
+    nodes.append_range(markov->nodes | std::views::transform(bindBack(executionNode, palette)));
+    return vbox(nodes);
+  }
+  if (const auto sequence = node.target<Sequence>(); sequence != nullptr) {
+    auto nodes = Elements{text("sequence")};
+    nodes.append_range(sequence->nodes | std::views::transform(bindBack(executionNode, palette)));
+    return vbox(nodes);
+  }
+  if (const auto limit = node.target<Limit>(); limit != nullptr) {
+    // auto nodes = Elements{text(std::format("<rulenode> (?/{})", limit->count))};
+    return ruleNode(limit->action, palette, limit->count);
+  }
+  if (const auto nolimit = node.target<NoLimit>(); nolimit != nullptr) {
+    return ruleNode(nolimit->action, palette);
+  }
+  return text("<unknown_execution_node>");
+}
+
+auto ConsoleApp::run(std::span<const std::string_view> args) noexcept -> int {
   load_palette(DEFAULT_PALETTE_FILE);
 
   load_model(std::ranges::size(args) >= 2 ? args[1] : DEFAULT_MODEL_FILE);
@@ -51,8 +132,7 @@ auto ConsoleApp::run([[maybe_unused]] std::span<const std::string_view> args) no
     return canvasFromImage(symbols_texture);
   });
   auto program_view = Renderer([&]() noexcept {
-    // TODO how the hell can I draw a program tree with my current typings O_O'
-    return text("<instruction_tree>");
+    return executionNode(model.program, symbols_palette);
   });
 
   init_grid(DEFAULT_GRID_EXTENT);
