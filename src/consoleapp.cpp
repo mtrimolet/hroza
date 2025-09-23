00bf0527e4f254e63c4ca02ae57845403a515b6a
Maxime Trimolet <maxime.trimolet@gmail.com>
2025-09-23 16:24:46 +0200

    split ui in components

--- src/consoleapp.cpp
+++ src/consoleapp.cpp
@@ -23,11 +23,121 @@ Decorator window_wrap(std::string title) {
   };
 }
 
+struct Controls {
+  bool tickrate_enabled = true;
+  int tickrate = DEFAULT_TICKRATE;
+  bool model_paused = false;
+  std::condition_variable pause_cv = {};
+  std::mutex pause_m = {};
+
+  std::function<void()> onReset = nullptr;
+  
+  void play_pause() {
+    {
+      auto l = std::lock_guard{ pause_m };
+      model_paused ^= true;
+    }
+    pause_cv.notify_one();
+  }
+
+  void reset() {
+    {
+      auto l = std::lock_guard{ pause_m };
+      model_paused = true;
+    }
+    pause_cv.notify_one();
+
+    onReset();
+  }
+
+  void sleep_missing(clk::time_point last_time) {
+    if (tickrate_enabled and tickrate != 0) {
+      const auto tickperiod = std::chrono::duration_cast<clk::duration>( 1000ms / tickrate );
+      const auto elapsed = clk::now() - last_time;
+      const auto missing = tickperiod - std::min(elapsed, tickperiod);
+      std::this_thread::sleep_for(missing);
+    }
+    {
+      auto l = std::unique_lock{ pause_m };
+      pause_cv.wait(l, [&paused = model_paused]{ return not paused; });
+    }
+  }
+};
+
+Component ControlsView(Controls& controls) {
+  return Container::Vertical({
+    Container::Horizontal({
+      Button("play/pause", std::bind_front(&Controls::play_pause, &controls)),
+      Button("reset", std::bind_front(&Controls::reset, &controls)),
+    }),
+    Slider<decltype(controls.tickrate)>({
+      .value = &controls.tickrate,
+      .direction = Direction::Right,
+      .on_change = nullptr,
+    })
+      | Renderer(border),
+    Container::Horizontal({
+      Renderer([&tickrate = controls.tickrate]{
+        return text(std::format("{} tick/s ", tickrate));
+      }),
+      Checkbox({
+        .label = "tickrate",
+        .checked = &controls.tickrate_enabled,
+        .transform = nullptr,
+      })
+        | Renderer(vcenter),
+    }),
+    Container::Horizontal({
+      // Button("previous", []{}),
+      Button("next", []{}),
+    })
+      | Renderer(hcenter),
+  });
+}
+
 template <typename T>
 struct GridScroll {
   T x;
   T y;
 };
+
+using namespace std::string_literals;
+
+Component WorldAndPotentials(const TracedGrid<char>& grid, const render::Palette& palette) {
+  struct Impl : ComponentBase {
+    std::vector<std::string> tabnames = {};
+    Components tabcomponents = {};
+    int tabselect = 0;
+    GridScroll<int> grid_scroll = { 0, 0 };
+
+    Impl(const TracedGrid<char>& grid, const render::Palette& palette) {
+      auto tabs = std::vector {
+        std::tuple { "World"s, Renderer([&grid, &palette]{ return render::grid(grid, palette); }) },
+      };
+
+      for (auto i : std::views::iota(0, 2)) {
+        tabs.push_back({
+          std::format("Potential {}", i),
+          Renderer([i]{ return text(std::format("<Potential {}>", i)); })
+        });
+      }
+
+      tabnames = std::views::keys(tabs) | std::ranges::to<std::vector>();
+      tabcomponents = std::views::values(tabs) | std::ranges::to<Components>();
+
+      Add(Container::Vertical({
+        Toggle(&tabnames, &tabselect),
+        Container::Tab(tabcomponents, &tabselect)
+          | Renderer([&grid_scroll = grid_scroll](Element e){
+              return e | focusPosition(grid_scroll.x, grid_scroll.y)
+                | vscroll_indicator | hscroll_indicator | frame
+                | border | center | flex_grow;
+          })
+      }));
+    }
+  };
+  return Make<Impl>(grid, palette);
+}
 
 auto ConsoleApp::operator()(std::span<const std::string_view> args) noexcept -> int {
   auto palettefile = DEFAULT_PALETTE_FILE;
@@ -59,43 +169,25 @@ auto ConsoleApp::operator()(std::span<const std::string_view> args) noexcept -> 
   auto grid = TracedGrid{extent, model.symbols[0]};
   if (model.origin) grid[grid.area().center()] = model.symbols[1];
 
-  auto tabs = std::vector<std::tuple<std::string, Component>> {
-    { "World", Renderer([&grid, &palette]{ return render::grid(grid, palette); }) },
-    { "Potential 0", Renderer([]{ return text("<Potential 0>"); }) },
-    { "Potential 1", Renderer([]{ return text("<Potential 1>"); }) },
+  auto controls = Controls {
+    .tickrate = DEFAULT_TICKRATE,
+    .onReset = [&grid, &extent, &model]{
+      reset(model.program);
+      grid = TracedGrid{extent, model.symbols[0]};
+      if (model.origin) grid[grid.area().center()] = model.symbols[1];
+    },
   };
-
-  auto tabnames = std::views::keys(tabs) | std::ranges::to<std::vector>();
-  auto tabcomponents = std::views::values(tabs) | std::ranges::to<Components>();
-  auto tabselect = 0;
-
-  auto tickrate_enabled = true;
-  auto tickrate = DEFAULT_TICKRATE;
-
-  auto grid_scroll = GridScroll { 0, 0 };
 
   auto screen = ScreenInteractive::Fullscreen();
   // screen.TrackMouse(false);
 
-  auto model_paused = false;
-  auto pause_cv = std::condition_variable{};
-  auto pause_m  = std::mutex {};
   auto program_runtime = [&](std::stop_token stop) mutable noexcept {
     auto last_time = clk::now();
     // swap the two next lines
     for (auto _ : model.program(grid)) {
       if (stop.stop_requested()) break;
 
-      if (tickrate_enabled and tickrate != 0) {
-        const auto tickperiod = std::chrono::duration_cast<clk::duration>( 1000ms / tickrate );
-        const auto elapsed = clk::now() - last_time;
-        const auto missing = tickperiod - std::min(elapsed, tickperiod);
-        std::this_thread::sleep_for(missing);
-      }
-      {
-        auto l = std::unique_lock{ pause_m };
-        pause_cv.wait(l, [&model_paused]{ return not model_paused; });
-      }
+      controls.sleep_missing(last_time);
 
       screen.RequestAnimationFrame();
       last_time = clk::now();
@@ -118,68 +210,15 @@ auto ConsoleApp::operator()(std::span<const std::string_view> args) noexcept -> 
       }),
       Renderer([&model, &palette]{
         return render::nodeRunner(model.program, palette)
-      // Renderer([]{
-      //   return text("<program>")
           | vscroll_indicator
           | yframe
           | window_wrap("program");
       }),
-      Container::Vertical({
-        Container::Horizontal({
-          Button("play/pause", [&]{
-            {
-              auto l = std::lock_guard{ pause_m };
-              model_paused ^= true;
-            }
-            pause_cv.notify_one();
-          }),
-          Button("reset", [&]{
-            {
-              auto l = std::lock_guard{ pause_m };
-              model_paused = true;
-            }
-            pause_cv.notify_one();
-
-            reset(model.program);
-            grid = TracedGrid{extent, model.symbols[0]};
-            if (model.origin) grid[grid.area().center()] = model.symbols[1];
-          }),
-        }),
-          Slider<decltype(tickrate)>({
-            .value = &tickrate,
-            .direction = Direction::Right,
-            .on_change = nullptr,
-          })
-            | Renderer(border),
-        Container::Horizontal({
-          Renderer([&tickrate]{
-            return text(std::format("{} tick/s ", tickrate));
-          }),
-          Checkbox({
-            .label = "tickrate",
-            .checked = &tickrate_enabled,
-            .transform = nullptr,
-          })
-            | Renderer(vcenter),
-        }),
-        Container::Horizontal({
-          // Button("previous", []{}),
-          Button("next", []{}),
-        })
-          | Renderer(hcenter),
-      })
+      ControlsView(controls)
         | Renderer(window_wrap("controls")),
     }),
     Renderer([]{ return separator(); }),
-    Container::Vertical({
-      Toggle(&tabnames, &tabselect),
-      Container::Tab(tabcomponents, &tabselect)
-        | Renderer(
-            focusPosition(grid_scroll.x, grid_scroll.y)
-              | vscroll_indicator | hscroll_indicator | frame
-              | border | center | flex_grow
-      )
-    })
+    WorldAndPotentials(grid, palette)
       | Renderer(flex_grow),
   })
     | Renderer(flex_grow);
